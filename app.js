64db51d75fc66fd2d664f65f5bf78b701c1ab349
elkake <gianco.marquez@gmail.com>
2023-02-12 19:25:44 -0300

    otro cambio

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import dotenv from 'dotenv'
 import Server from './models/server.js'
+import mongoose from 'mongoose'
 // import { fileURLToPath } from 'url'
 // import { dirname } from 'path'
 
 // const dirURL = dirname(fileURLToPath(import.meta.url))
 
 dotenv.config()
+mongoose.set('strictQuery', true)
 const server = new Server()
 
 server.listen()
