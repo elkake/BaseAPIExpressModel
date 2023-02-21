095fd6b4b54a1ab31d111ddab26868eaa1fe3cb4
elkake <gianco.marquez@gmail.com>
2023-02-21 10:30:57 -0300

    google verify

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
 import { request, response } from 'express'
 import Usuario from '../models/usuario.js'
 import bcryptjs from 'bcryptjs'
-import {generarJWT} from '../helpers/generarJWT.js'
+import { generarJWT } from '../helpers/generarJWT.js'
+import { googleVerify } from '../helpers/google-verify.js'
 
 export const login = async (req = request, res = response) => {
   const { correo, password } = req.body
@@ -9,34 +10,75 @@ export const login = async (req = request, res = response) => {
   try {
     //  verificar si el email existe
     const usuario = await Usuario.findOne({ correo })
-    if(!usuario){
-        return res.status(400).json({
-            msg:'usuario/password no son correctos -correo'
-        })
+    
+    if (!usuario) {
+      return res.status(400).json({
+        msg: 'usuario/password no son correctos -correo'
+      })
     }
     // si el usuario esta activo
-    if(!usuario.estado){
-        return res.status(400).json({
-            msg:'usuario/password no son correctos -estado false'
-        })
+    if (!usuario.estado) {
+      return res.status(400).json({
+        msg: 'usuario/password no son correctos -estado false'
+      })
     }
     // verificar la contraseña
 
-    const validPassword=bcryptjs.compareSync(password,usuario.password)
-    if(!validPassword){
-        return res.status(400).json({
-            msg:'usuario/password no son correctos -password'
-        })
+    const validPassword = bcryptjs.compareSync(password, usuario.password)
+    if (!validPassword) {
+      return res.status(400).json({
+        msg: 'usuario/password no son correctos -password'
+      })
     }
     // generar el jwt
 
-    const token=await generarJWT(usuario.id)
+    const token = await generarJWT(usuario.id)
 
-
-    res.json({ usuario,token })
+    res.json({ usuario, token })
   } catch (e) {
     return res.status(500).json({
       msg: 'Hable con el admin'
+    })
+  }
+}
+
+export const googleSignIn = async (req = request, res = response) => {
+  const { id_token } = req.body
+
+  try {
+    const { nombre, imagen, correo } = await googleVerify(id_token)
+
+    let usuario = await Usuario.findOne({ correo })
+    if (!usuario) {
+      const data = {
+        nombre,
+        correo,
+        password: ':P',
+        imagen,
+        google: true,
+        rol: 'USER_ROLE'
+      }
+// crea ell usuario
+      usuario = new Usuario(data)
+      await usuario.save()
+    }
+// revisa el estado
+    if (!usuario.estado) {
+      return res.status(401).json({
+        msg: 'Hable con el administrador, usuario bloqueado'
+      })
+    }
+
+    // generara jwt
+    const token = await generarJWT(usuario.id)
+
+    res.json({
+      usuario,
+      token
+    })
+  } catch (error) {
+    res.status(500).json({
+      msg: 'el correo no se pudo validar'
     })
   }
 }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,20 +1,27 @@
 import { Router } from 'express'
 import { check } from 'express-validator'
-import { login } from '../controllers/auth.js'
+import { googleSignIn, login } from '../controllers/auth.js'
 import validarCampos from '../middlewares/validar-campos.js'
-
 
 const router = Router()
 
 router.post(
   '/login',
   [
-    
     check('correo', 'el Correo es obligatorio').isEmail(),
     check('password', 'La contraseña es obligatoria').not().isEmpty(),
     validarCampos
   ],
   login
+)
+
+router.post(
+  '/google',
+  [
+    check('id_token', 'el Id_token es necesario').not().isEmpty(),
+    validarCampos
+  ],
+  googleSignIn
 )
 
 export default router
